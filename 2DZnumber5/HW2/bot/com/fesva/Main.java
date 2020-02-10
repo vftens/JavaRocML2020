d4615185406833563d33e7b8f2bf49e2b9596aa2
vftens <aico@ya.ru>
2020-02-10 18:19:58 +0300

    Try working
    Try working - minimally needed try

--- 2DZnumber5/HW2/bot/com/fesva/Main.java
+++ 2DZnumber5/HW2/bot/com/fesva/Main.java
@@ -5,32 +5,91 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public  class Main {
+public class Main {
+
+    private static boolean debug = false; //true;
 
     private static ArrayList<String> deals = new ArrayList<>();
     public SimpleBot simpleBot;
+
+    private static BufferedReader reader;
+    private static String[] parts;
+    private static String inputS;
+    private static boolean hasIndex = false; // index
+    private static boolean hasText; // text
+    private static boolean hasText1 = false; // text 2-j string
+    private static int n = 1;
+    private static String s = "";
 
     public static ArrayList<String> getDeals() {
         return deals;
     }
 
+    public static boolean HasIndex() throws Exception  {
+        if (parts[1] != null) {
+            if (parts[1].matches("\\d+")) {
+                hasIndex = true;
+                n = Integer.parseInt(parts[1]);
+                return true;
+            } else {
+                hasIndex = false;
+                return false;
+            }
+        }
+        else {
+            return  false;
+        }
+    }
+
+    public static boolean HasText() throws Exception {
+        if (parts[1] != null) {
+            if (parts[1].matches("\\w+")) {
+                hasText1 = true;
+                s = parts[1];
+                return true;
+            } else {
+                hasText1 = false;
+                return false;
+            }
+        }
+        else{
+            return false;
+        }
+    }
+
+    public static boolean HasText2() throws Exception {
+        //s = parts[2];
+        if (parts[2] != null) {
+            if (parts[2].matches("\\w+")) {
+                hasText = true;
+                s = parts[2];
+                return true;
+            } else {
+                hasText = false;
+                return false;
+            }
+        }else {
+            return false;
+        }
+    }
+
+    public static void TryTensorflow() throws Exception {
+        System.out.println("Switch on Tensorflow?[Y/N]");
+        inputS = reader.readLine().trim();
+        if (inputS.equals("Y") || inputS.equals("y")) {
+            HelloTensorFlow htf = new HelloTensorFlow();
+            htf.main(""); // ?????????
+        }
+        // htf.wait(1);
+        // TODO: adding working with TF and GPT2
+    }
+
     public static void main(String[] args) throws Exception {
-
-        String[] parts;
-        String inputS;
-        boolean hasIndex = false; // ??? ?????
-        boolean hasText; // ??? ??????
-        boolean hasText1 = false; // ??? ?????? ?? 2-? ?????
-
         SimpleBot simpleBot = new SimpleBot();
-
         // write your code here
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
+        reader = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
             //simpleBot.
-            String s = "";
-            int n = 1;
             System.out.println("Please, Input Your Command:");
             //String
             inputS = reader.readLine().trim();
@@ -38,96 +97,80 @@ public  class Main {
             try {
                 parts = inputS.split("\\s");
                 try {
-
-                    if (parts[1].matches("\\d+")) {
-                        hasIndex = true;
-                        n = Integer.parseInt(parts[1]);
+                    if (debug) {
+                        System.out.println("Before HasIndex");
                     }
-                    else {
-                        hasIndex = false;
-                    }
-                } catch (Exception e) {
-                    hasIndex = false;
-
-                    try {
-                        //s = parts[1];
-                        if (parts[1].matches("\\w+")) {
-                            hasText1 = true;
-                            s = parts[1];
+                    if (!HasIndex()) {
+                        if (debug) {
+                            System.out.println("Inside HasIndex Then");
                         }
-                        else {
-                            hasText1 = false;
+                        if (!HasText()) {
+                            //s = parts[1];
+                            if (debug) {
+                                System.out.println("Inside HasText Then");
+                            }
                         }
-                    } catch (Exception e1) {
-                        hasText1 = false;
                     }
-                }
-                try {
                     //String
-                    //s = parts[2];
-                    if (parts[2].matches("\\w+")) {
-                        hasText = true;
-                        s = parts[2];
+                    if (!HasText2()) {
+                        if (debug) {
+                            System.out.println("Inside HasText2 Then");
+                        }
                     }
-                    else {
-                        hasText = false;
-                    }
-                } catch (Exception e) {
-                    hasText = false;
+                }catch(Exception e){
+                    // Nothing to say
                 }
-
                 // Commands
-                if (parts[0].equals("LIST")) {
-                    int i = 0;
-                    for (String item : deals) {
-                        i++;
-                        System.out.println(
-                                java.lang.String.format("%d %s", i, item));
-                    }
-                } else if (parts[0].equals("ADD")) {
-                    if (hasIndex && hasText) {
-                        deals.add(n, s);
-                    } else if (hasIndex) {
-                        //if (debug) System.out.println("Only n ");//
-                        if (deals.add(s)) System.out.println("Ok ");
-                        else System.out.println("Not added String " + s);
-                    } else if (hasText) {
-                        if (deals.add(s)) System.out.println("Ok ADDing String ");
-                    } else if (hasText1) {
-                        if (deals.add(s)) System.out.println("Ok ADD String ");
-                    } else System.out.println("You must enter your specifications for ADD ");
+                if (parts[0] != null) {
+                    if (parts[0].equals("LIST")) {
+                        int i = 0;
+                        for (String item : deals) {
+                            i++;
+                            System.out.println(
+                                    java.lang.String.format("%d %s", i, item));
+                        }
+                    } else if (parts[0].equals("ADD")) {
+                        if (hasIndex && hasText) {
+                            deals.add(n, s);
+                            if (debug) {
+                                System.out.println("ADD N S ");//
+                            }
+                        } else if (hasIndex) {
+                            //if (debug) System.out.println("Only n ");//
+                            if (deals.add(s)) System.out.println("Ok ");
+                            else System.out.println("Not added String " + s);
+                        } else if (hasText) {
+                            if (deals.add(s)) {
+                                System.out.println("Ok ADDing String ");
+                            }
+                        } else if (hasText1) {
+                            if (deals.add(s)) System.out.println("Ok ADD String ");
+                        } else System.out.println("You must enter your specifications for ADD ");
 
-                } else if (parts[0].equals("EDIT")) {
-                    if (hasIndex && hasText) {
-                        deals.set(n, s);
-                    } else System.out.println("You must enter your specifications for EDIT ");
+                    } else if (parts[0].equals("EDIT")) {
+                        if (hasIndex && hasText) {
+                            deals.set(n, s);
+                        } else System.out.println("You must enter your specifications for EDIT ");
 
-                } else if (parts[0].equals("DELETE")) {
-                    if (hasIndex) {
-                        deals.remove(n);
-                    } else System.out.println("You must enter your number for DELETE ");
+                    } else if (parts[0].equals("DELETE")) {
+                        if (hasIndex) {
+                            deals.remove(n);
+                        } else System.out.println("You must enter your number for DELETE ");
+                    } else System.out.println("Sorry, Unknown Command.");
+
                 }
-
             } catch (NumberFormatException e) {
                 e.printStackTrace();
             } catch (IndexOutOfBoundsException e) {
                 System.out.println("Index Out of Bounds Exception, Please Use Appropriate Index. " + e);
-
             } catch (Exception e) {
                 System.out.println("MY Exception " + e);
             } finally {
-                System.out.println(simpleBot.sayInReturn(inputS, true)); // ?????????
+                System.out.println(simpleBot.sayInReturn(inputS, true)); //     //sayInReturn
             }
-
         } // end while
-        HelloTensorFlow htf = new HelloTensorFlow();
-        htf.main(""); // ?????????
-        // htf.wait(1);
-        // TODO: adding working with TF and GPT2
-
+        TryTensorflow();
     }
-
-    //sayInReturn
 
     public SimpleBot getSimpleBot() {
         SimpleBot simpleBot = new SimpleBot();
