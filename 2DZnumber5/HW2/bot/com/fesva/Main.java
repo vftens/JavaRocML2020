8bb4a661dee80dc01eb3727513a00ec840faac2c
vftens <aico@ya.ru>
2020-02-22 19:12:35 +0300

    Update Main.java
    Update Main.java

--- 2DZnumber5/HW2/bot/com/fesva/Main.java
+++ 2DZnumber5/HW2/bot/com/fesva/Main.java
@@ -13,7 +13,11 @@ public class Main {
     public static Integer getIndex(String[] inputTokens) {
         if (inputTokens.length > 1 && inputTokens[1].matches("\\d{1,5}")) {
             int myindex = Integer.parseInt(inputTokens[1]);
-            return myindex; //Math.min(myindex, deals.size() );
+
+            if (myindex > deals.size()) {
+                myindex = deals.size() - 1;
+            }
+            return myindex;
         }
         return null;
     }
@@ -41,80 +45,58 @@ public class Main {
             //String
             String inputS = reader.readLine().trim();
             if (inputS.equals("EXIT")) break;
-            try {
-                String[] parts = inputS.split("\\s+", 3);
 
-                String command = null;
-                Integer index = 0;
-                String text = null;
-                try {
-                    command = parts[0];
-                    if (getIndex(parts) != null) {
-                        index = getIndex(parts);
-                        if (index > deals.size()) {
-                            index = deals.size() - 1;
-                        }
+            String[] parts = inputS.split("\\s+", 3);
+
+            String command = parts[0];
+            Integer index = getIndex(parts);
+            String text = getText(parts);
+            // Commands
+            switch (command) {
+                case "LIST":
+                    int i = 0;
+                    for (String item : deals) {
+                        i++;
+                        System.out.printf("%d %s%n", i, item);
                     }
-                    text = getText(parts);
-
-                } catch (Exception e) {
-                    if (debug) System.out.println("Exception, Nothing to say " + e);// Nothing to say
-                }
-                // Commands
-                switch (command) {
-                    case "LIST":
-                        int i = 0;
-                        for (String item : deals) {
-                            i++;
-                            System.out.printf("%d %s%n", i, item);
+                    break;
+                case "ADD":
+                    if (index == null) {
+                        deals.add(text);
+                    } else if (text != null) {
+                        deals.add(index, text);
+                    } else {
+                        System.out.println("You must enter your specifications for ADD ");
+                    }
+                    break;
+                case "EDIT":
+                    if (index != null && text != null) {
+                        //if (hasIndex && hasText)
+                        deals.set(index, text); //(n, s);
+                    } else {
+                        System.out.println("You must enter your specifications for EDIT ");
+                    }
+                    break;
+                case "DELETE":
+                    if (index != null) {
+                        deals.remove((int) index);
+                        if (debug) {
+                            System.out.println("Delete " + index);
                         }
-                        break;
-                    case "ADD":
-                        if (index == null) {
-                            deals.add(text);
-                        } else if (text != null) {
-                            deals.add(index, text);
-                        } else {
-                            System.out.println("You must enter your specifications for ADD ");
-                        }
-
-                        break;
-                    case "EDIT":
-                        if (index != null && text != null) {
-                            //if (hasIndex && hasText)
-                            deals.set(index, text); //(n, s);
-                        } else System.out.println("You must enter your specifications for EDIT ");
-                        break;
-                    case "DELETE":
-                        if (index != null && deals.size() <= 2) {
-                            deals.remove((int) index);
-                            if (debug) {
-                                System.out.println("Delete " + index);
-                            }
-                        } else System.out.println("You must enter your number for DELETE ");
-                        break;
-                    default:
-                        System.out.println("Sorry, Unknown Command.");
-                        break;
-                }
-            } catch (NumberFormatException e) {
-                e.printStackTrace();
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println("Index Out of Bounds Exception, Please Use Appropriate Index. " + e);
-            } catch (Exception e) {
-                System.out.println("MY Exception " + e);
-            } finally {
-                System.out.println(simpleBot.sayInReturn(inputS, true)); //     //sayInReturn
+                    } else {
+                        System.out.println("You must enter your number for DELETE ");
+                    }
+                    break;
+                default:
+                    System.out.println("Sorry, Unknown Command.");
+                    break;
             }
+            System.out.println(simpleBot.sayInReturn(inputS, true)); //     //sayInReturn
         } // end while
     }
 
     public static SimpleBot getSimpleBot() {
         SimpleBot simpleBot = new SimpleBot();
         return simpleBot;
-    }
-
-    public static ArrayList<String> getDeals() {
-        return deals;
     }
 }
