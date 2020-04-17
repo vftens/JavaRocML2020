c62b4e4902aa8c6c22101e33606bd25681394ae0
vftens <aico@ya.ru>
2020-04-17 21:08:11 +0300

    try init
    try init

--- 08_ExceptionsDebuggingAndTesting/hw1/ConsoleCustomerList/ConsoleCustomerList/src/Main.java
+++ 08_ExceptionsDebuggingAndTesting/hw1/ConsoleCustomerList/ConsoleCustomerList/src/Main.java
@@ -12,22 +12,28 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CustomerStorage executor = new CustomerStorage();
-        for (; ; ) {
-            String command = scanner.nextLine();
-            String[] tokens = command.split("\\s+", 2);
-            if (tokens[0].equals("add")) {
-                executor.addCustomer(tokens[1]);
-            } else if (tokens[0].equals("list")) {
-                executor.listCustomers();
-            } else if (tokens[0].equals("remove")) {
-                executor.removeCustomer(tokens[1]);
-            } else if (tokens[0].equals("count")) {
-                System.out.println("There are " + executor.getCount() + " customers");
-            } else if (tokens[0].equals("help")) {
-                System.out.println(helpText);
-            } else {
-                System.out.println(commandError);
+        try {
+            for (; ; ) {
+                String command = scanner.nextLine();
+                String[] tokens = command.split("\\s+", 2);
+                if (tokens[0].equals("add")) {
+                    executor.addCustomer(tokens[1]);
+                } else if (tokens[0].equals("list")) {
+                    executor.listCustomers();
+                } else if (tokens[0].equals("remove")) {
+                    executor.removeCustomer(tokens[1]);
+                } else if (tokens[0].equals("count")) {
+                    System.out.println("There are " + executor.getCount() + " customers");
+                } else if (tokens[0].equals("help")) {
+                    System.out.println(helpText);
+                } else {
+                    System.out.println(commandError);
+                }
             }
+        } catch (ArrayIndexOutOfBoundsException e1){
+            System.out.println("EXCEPTION " + e1);
+        } catch(Exception e){
+            System.out.println("Exception " + e);
         }
     }
 }
