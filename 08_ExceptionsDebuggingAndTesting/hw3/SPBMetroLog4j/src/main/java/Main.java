767fe6687f0bef3cd5beadc918b36248e820479a
vftens <aico@ya.ru>
2020-06-09 19:53:03 +0300

    working variant
    working variant. Not starting metro. Need to change starting point

--- 08_ExceptionsDebuggingAndTesting/hw3/SPBMetroLog4j/src/main/java/Main.java
+++ 08_ExceptionsDebuggingAndTesting/hw3/SPBMetroLog4j/src/main/java/Main.java
@@ -1,5 +1,9 @@
 import core.Line;
 import core.Station;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -7,81 +11,97 @@ import org.json.simple.parser.JSONParser;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
+//import java.util.logging.Logger;
 
-public class Main
-{
-    private static String dataFile = "SPBMetro/src/main/resources/map.json";
+public class Main {
+    private static Logger logger;
+    private static final Marker INPUT_OK = MarkerManager.getMarker("INPUT_OK");
+    private static final Marker INPUT_ERROR = MarkerManager.getMarker("INPUT_ERROR");
+    private static final Marker EXCEPTION = MarkerManager.getMarker("EXCEPTION");
+
+    private static String dataFile = "src/main/resources/map.json";
     private static Scanner scanner;
 
     private static StationIndex stationIndex;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
+        logger = LogManager.getRootLogger();
+        logger.info("Запуск приложения" );
+
         RouteCalculator calculator = getRouteCalculator();
 
         System.out.println("Программа расчёта маршрутов метрополитена Санкт-Петербурга\n");
-        scanner = new Scanner(System.in);
-        for(;;)
-        {
-            Station from = takeStation("Введите станцию отправления:");
-            Station to = takeStation("Введите станцию назначения:");
 
-            List<Station> route = calculator.getShortestRoute(from, to);
-            System.out.println("Маршрут:");
-            printRoute(route);
+        try {
+            scanner = new Scanner(System.in);
+            for (; ; ) {
+                Station from = takeStation("Введите станцию отправления:");
+                logger.info(INPUT_OK, "Станция отправления: " + from.getName());
 
-            System.out.println("Длительность: " +
-                RouteCalculator.calculateDuration(route) + " минут");
+                Station to = takeStation("Введите станцию назначения:");
+                logger.info(INPUT_OK, "Станция назначения: " + to.getName());
+
+                List<Station> route = calculator.getShortestRoute(from, to);
+
+                System.out.println("Маршрут:");
+                logger.info("Проложен маршрут:" );
+                printRoute(route);
+
+                System.out.println("Длительность: " +
+                        RouteCalculator.calculateDuration(route) + " минут");
+                logger.info("Длительность маршрута: " +
+                        RouteCalculator.calculateDuration(route) + " минут");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            logger.error(EXCEPTION, "Ошибка: " + Arrays.toString(ex.getStackTrace()));
         }
     }
 
-    private static RouteCalculator getRouteCalculator()
-    {
+    private static RouteCalculator getRouteCalculator() {
         createStationIndex();
         return new RouteCalculator(stationIndex);
     }
 
-    private static void printRoute(List<Station> route)
-    {
+    private static void printRoute(List<Station> route) {
         Station previousStation = null;
-        for(Station station : route)
-        {
-            if(previousStation != null)
-            {
+        for (Station station : route) {
+            if (previousStation != null) {
                 Line prevLine = previousStation.getLine();
                 Line nextLine = station.getLine();
-                if(!prevLine.equals(nextLine))
-                {
+                if (!prevLine.equals(nextLine)) {
                     System.out.println("\tПереход на станцию " +
-                        station.getName() + " (" + nextLine.getName() + " линия)");
+                            station.getName() + " (" + nextLine.getName() + " линия)");
+                    logger.info("\tПереход на станцию " +
+                            station.getName() + " (" + nextLine.getName() + " линия)");
                 }
             }
             System.out.println("\t" + station.getName());
+            logger.info("\t" + station.getName());
+
             previousStation = station;
         }
     }
 
-    private static Station takeStation(String message)
-    {
-        for(;;)
-        {
+    private static Station takeStation(String message) {
+        for (; ; ) {
             System.out.println(message);
             String line = scanner.nextLine().trim();
             Station station = stationIndex.getStation(line);
-            if(station != null) {
+            if (station != null) {
                 return station;
             }
             System.out.println("Станция не найдена :(");
+            logger.info(INPUT_ERROR, "Станция не найдена: " + line);
         }
     }
 
-    private static void createStationIndex()
-    {
+    private static void createStationIndex() {
         stationIndex = new StationIndex();
-        try
-        {
+        try {
             JSONParser parser = new JSONParser();
             JSONObject jsonData = (JSONObject) parser.parse(getJsonFile());
 
@@ -93,14 +113,14 @@ public class Main
 
             JSONArray connectionsArray = (JSONArray) jsonData.get("connections");
             parseConnections(connectionsArray);
-        }
-        catch(Exception ex) {
-            ex.printStackTrace();
+        } catch (Exception ex) {
+            logger.error(EXCEPTION, "Ошибка в JSON-файле: \n" +
+                    Arrays.toString(ex.getStackTrace()).replaceAll(",", "\n"));
+//            ex.printStackTrace();
         }
     }
 
-    private static void parseConnections(JSONArray connectionsArray)
-    {
+    private static void parseConnections(JSONArray connectionsArray) {
         connectionsArray.forEach(connectionObject ->
         {
             JSONArray connection = (JSONArray) connectionObject;
@@ -112,10 +132,10 @@ public class Main
                 String stationName = (String) itemObject.get("station");
 
                 Station station = stationIndex.getStation(stationName, lineNumber);
-                if(station == null)
-                {
-                    throw new IllegalArgumentException("core.Station " +
-                        stationName + " on line " + lineNumber + " not found");
+                if (station == null) {
+                    String errMsg = "Ошибка: core.Station " + stationName + " on line " + lineNumber + " not found";
+                    logger.error(EXCEPTION, errMsg);
+                    throw new IllegalArgumentException(errMsg);
                 }
                 connectionStations.add(station);
             });
@@ -123,8 +143,7 @@ public class Main
         });
     }
 
-    private static void parseStations(JSONObject stationsObject)
-    {
+    private static void parseStations(JSONObject stationsObject) {
         stationsObject.keySet().forEach(lineNumberObject ->
         {
             int lineNumber = Integer.parseInt((String) lineNumberObject);
@@ -139,8 +158,7 @@ public class Main
         });
     }
 
-    private static void parseLines(JSONArray linesArray)
-    {
+    private static void parseLines(JSONArray linesArray) {
         linesArray.forEach(lineObject -> {
             JSONObject lineJsonObject = (JSONObject) lineObject;
             Line line = new Line(
@@ -151,15 +169,15 @@ public class Main
         });
     }
 
-    private static String getJsonFile()
-    {
+    private static String getJsonFile() {
         StringBuilder builder = new StringBuilder();
         try {
             List<String> lines = Files.readAllLines(Paths.get(dataFile));
             lines.forEach(line -> builder.append(line));
-        }
-        catch (Exception ex) {
-            ex.printStackTrace();
+        } catch (Exception ex) {
+            logger.error(EXCEPTION, "Не найден JSON-файл: " + ex.getMessage() + "\n" +
+                    Arrays.toString(ex.getStackTrace()).replaceAll(",", "\n"));
+//            ex.printStackTrace();
         }
         return builder.toString();
     }
--- 08_ExceptionsDebuggingAndTesting/hw3/SPBMetroLog4j/src/test/java/RouteCalculatorTest.java
+++ 08_ExceptionsDebuggingAndTesting/hw3/SPBMetroLog4j/src/test/java/RouteCalculatorTest.java
@@ -3,41 +3,189 @@
 import core.Line;
 import core.Station;
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
+/**
+ * Metro scheme that used in tests:
+ * <pre>{@code
+ *
+ *       (Line 1)
+ *      Station A - "Гостиный двор"
+ *          ↓
+ *     "Маяковская" Station B → "Площадь Восстания" Station E → Station G → Station H (Line 2)
+ *          ↓
+ *      Station C
+ *          ↓
+ *      Station D → Station J → Station K → Station Q (Line 3)
+ *                                 ↓
+ *                              Station L
+ *                              (Line 4)
+ *
+ * }</pre>
+ */
+
 public class RouteCalculatorTest extends TestCase {
-    List<Station> route;
-    Station from;
-    Station to;
+    Line[] line;
+    Station[][] station;
+
+    StationIndex testStationIndex;
+    RouteCalculator testCalculator;
+    List<Station> testRoute;
+
+    private List<Station> route;
+    private Station from;
+    private Station to;
+
+    //private Station lineOne;
+    //private Station lineTwo;
+    private Station stationA;
+    private Station stationB;
+    //private Station stationC;
+    private Station Station3;
+    Station A, B, C, D, E, K, F, G, X, Y, Z;
+    private static StationIndex stationIndex; // = null;
+    private RouteCalculator routeCalculator; // = new RouteCalculator(stationIndex);;
 
     @Override
     protected void setUp() throws Exception {
+        line = new Line[3];
+        station = new Station[3][3]; // [N линии] [N станции на этой линии]
+
+        testStationIndex = new StationIndex();
+        testCalculator = new RouteCalculator(testStationIndex);
+        testRoute = new ArrayList<>();
+
+        for (int l = 0; l < 3; l++) {
+
+            line[l] = new Line(l + 1, "Line " + (l + 1));
+            testStationIndex.addLine(line[l]); // добавление линии в индекс
+
+            for (int s = 0; s < 3; s++) {
+
+                station[l][s] = new Station("Station " + (s + 1) + " on line " + (l + 1), line[l]);
+                line[l].addStation(station[l][s]); // привязка станции к линии
+                testStationIndex.addStation(station[l][s]); // добавление станции в индекс
+            }
+        }
+        // добавление переходов в индекс
+        testStationIndex.addConnection(new ArrayList<>(Arrays.asList(station[0][1], station[1][0])));
+        testStationIndex.addConnection(new ArrayList<>(Arrays.asList(station[1][2], station[2][1])));
+
+        InitLineTest il = new InitLineTest(); // Start BlueLine from Igor
+
+        Line lineA = new Line(1, "lineA");
+        Line lineX = new Line(2, "lineX");
+
         route = new ArrayList<>();
+        stationIndex = new StationIndex();
+
+        routeCalculator = new RouteCalculator(stationIndex);
+
         Line line1 = new Line(1, "Первая");
         Line line2 = new Line(2, "Вторая");
         Line line3 = new Line(3, "Третья");
-        route.add(new Station("Петровская", line1));
-        route.add(new Station("Арбузная", line1));
-        route.add(new Station("Морковная", line2));
-        route.add(new Station("Яблочная", line2));
+        route.add(new Station("K", line1));
+        route.add(new Station("F", line1));
+        route.add(new Station("G", line2));
+        route.add(new Station("D", line2));
 
         from = new Station("Маяковская", line3);
         to = new Station("Гостиный двор", line3);
+        Station3 = new Station("Площадь Восстания", line1);
+        E = Station3;
+        Z = new Station("Проспект Ветеранов", line1);
+        stationA = from;
+        stationB = to;
+
+        //StationIndex stationIndex = new StationIndex();
+        /*
+        for (Station station : Arrays.asList(A, B, C, X, Y, Z)) {
+            stationIndex.addStation(station);
+        }
+
+         */
+
+        for (Line line : Arrays.asList(lineA, lineX)) {
+            stationIndex.addLine(line);
+        }
+
+
+        //route = routeCalculator.getShortestRoute(from, to);
+        /*
+        List<Station> myto = null;
+        myto.add(to);
+        myto.add(from);
+        stationIndex.addConnection((List<Station>) myto);
+        stationIndex.addStation(from); //lineOne);
+        stationIndex.addStation(to);
+        
+         */
         super.setUp();
     }
 
-    public void testgetShortestRoute()
-    {
-        route = RouteCalculator.getShortestRoute(from,  to);
-        double actual = RouteCalculator.calculateDuration(route);
-        double expected = 2.5;
-        assertEquals(expected, actual);
+    @Test
+    public void testredLine() {
+        Line redLine = new Line(2, "Red");
+        Station A = new Station("A", redLine);
+        Station B = new Station("B", redLine);
+        Station C = new Station("C", redLine);
+        Station D = new Station("D", redLine);
+
+        redLine.addStation(A);
+        redLine.addStation(B);
+        redLine.addStation(C);
+        redLine.addStation(D);
+
+
+        stationIndex.addLine(redLine);
+        stationIndex.addStation(A);
+        stationIndex.addStation(B);
+        stationIndex.addStation(C);
+        stationIndex.addStation(D);
     }
 
-    public void testgetRouteWithOneConnection()
-    {
+    @Test
+    public void testblueLine() {
+        Line blueLine = new Line(1, "Blue");
+        Station A = new Station("A", blueLine);
+        Station B = new Station("B", blueLine);
+        Station C = new Station("C", blueLine);
+        Station D = new Station("D", blueLine);
+
+        blueLine.addStation(A);
+        blueLine.addStation(B);
+        blueLine.addStation(C);
+        blueLine.addStation(D);
+
+        stationIndex.addLine(blueLine);
+        stationIndex.addStation(A);
+        stationIndex.addStation(B);
+        stationIndex.addStation(C);
+        stationIndex.addStation(D);
+    }
+
+    /*
+    @Test
+    public void testgetShortestRoute() {
+        ArrayList<Station> actual = new ArrayList<>();
+        actual.add(stationA);
+        actual.add(stationB);
+        routeCalculator = new RouteCalculator(stationIndex);
+        route = routeCalculator.getShortestRoute(stationA, stationB);
+        double actual1 = routeCalculator.calculateDuration(route);
+        double expected = 2.5;
+        assertEquals(expected, actual1);
+    }
+
+     */
+
+    public void testgetRouteWithOneConnection() {
 
     }
 
@@ -45,18 +193,143 @@ public class RouteCalculatorTest extends TestCase {
     {
         StationIndex stationIndex1 = null;
         RouteCalculator ins = new RouteCalculator(stationIndex1);
+        routeCalculator = new RouteCalculator(stationIndex);
         System.out.println(ins);
-
     }
 
-    public void testCalculateDuration(){
+    // тест метода расчета времени поездки
+    public void testCalculateDurationMy() {
+
+        // Самый длинный маршрут с 2 пересадками
+        testRoute.add(testStationIndex.getStation("Station 1 on line 1"));
+        testRoute.add(testStationIndex.getStation("Station 2 on line 1"));
+        testRoute.add(testStationIndex.getStation("Station 1 on line 2"));
+        testRoute.add(testStationIndex.getStation("Station 2 on line 2"));
+        testRoute.add(testStationIndex.getStation("Station 3 on line 2"));
+        testRoute.add(testStationIndex.getStation("Station 2 on line 3"));
+        testRoute.add(testStationIndex.getStation("Station 3 on line 3"));
+
+        double actual = RouteCalculator.calculateDuration(testRoute);
+        double expected = 17.0;
+
+        assertEquals(expected, actual);
+    }
+
+    // тест метода построения маршрута - по времени поездки - маршрут без переходов
+    public void testTimeOfRouteOnTheLine() throws NullPointerException {
+
+        List<Station> testRoute = testCalculator.getShortestRoute(
+                testStationIndex.getStation("Station 1 on line 1"),
+                testStationIndex.getStation("Station 3 on line 1"));
+
+        double actual = RouteCalculator.calculateDuration(testRoute);
+        double expected = 2 * 2.5; // 2 прогона
+
+        assertEquals(expected, actual);
+    }
+
+    // тест метода построения маршрута - по времени поездки - маршрут  с 1 переходом
+    public void testTimeOfRouteWithOneConnections() throws NullPointerException {
+
+        List<Station> testRoute = testCalculator.getShortestRoute(
+                testStationIndex.getStation("Station 1 on line 1"),
+                testStationIndex.getStation("Station 2 on line 2"));
+
+        double actual = RouteCalculator.calculateDuration(testRoute);
+        double expected = 2 * 2.5 + 3.5; // 2 прогона, 1 переход
+
+        assertEquals(expected, actual);
+    } // why zero, not expected?
+
+    // тест метода построения маршрута - по времени поездки - маршрут  с 2 переходами
+    public void testTimeOfRouteWithTwoConnections() throws NullPointerException {
+
+        List<Station> testRoute = testCalculator.getShortestRoute(
+                testStationIndex.getStation("Station 1 on line 1"),
+                testStationIndex.getStation("Station 2 on line 3"));
+
+        double actual = RouteCalculator.calculateDuration(testRoute);
+        double expected = 3 * 2.5 + 2 * 3.5; // 3 прогона, 2 перехода
+
+        assertEquals(expected, actual);
+    } // why zero, not expected?
+
+    public void testGetShortestRouteShouldNoNull() throws NullPointerException {
+
+        assertNotNull(testCalculator.getShortestRoute(
+                testStationIndex.getStation("Station 1 on line 1"),
+                testStationIndex.getStation("Station 3 on line 3")));
+    }
+
+    public void testCalculateDuration() {
         double actual = RouteCalculator.calculateDuration(route);
         double expected = 8.5;
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void test_calculate_duration_with_equal_stations() {
+        ArrayList<Station> actual = new ArrayList<>();
+        actual.add(stationA);
+        actual.add(stationA);
+        RouteCalculator calculator = new RouteCalculator(stationIndex);
+        List<Station> route = calculator.getShortestRoute(stationA, stationA);
+        double actual1 = RouteCalculator.calculateDuration(route);
+        assertEquals(0.0, actual1);
+    }
+
+    /*
+    @Test
+    public void test_get_shortest_route_on_one_line() {
+        List<Station> expected = Arrays.asList(Z, A, E);
+        //z = ;
+        List<Station> actual = routeCalculator.getShortestRoute(Z, E);
+        assertEquals(expected, actual);
+    }
+
+    //
+    public void test_distance_to_same_station() { //...
+
+    }
+
+    public void test_stations_next_to_each_other_on_single_line() { //...
+
+    }
+
+    public void test_opposite_stations_on_single_line() { //...
+
+    }
+
+    public void test_opposite_stations_with_two_transfers() { //...
+
+    }
+
+     */
+
+    /*
+    public void test_opposite_stations_with_one_transfer() { //...
+        ArrayList<Station> actual = new ArrayList<>();
+        actual.add(stationA);
+        actual.add(Station3);
+        RouteCalculator calculator = new RouteCalculator(stationIndex);
+        List<Station> route = calculator.getShortestRoute(stationA, Station3);
+        double actual1 = RouteCalculator.calculateDuration(route);
+        assertEquals(3.5, actual1);
+    }
+    */
 
     @Override
     protected void tearDown() throws Exception {
         super.tearDown();
     }
 }
+
+
+/**
+ * забытая схема - а нужна ли она?
+ * A - B ----- E      (line1: Z - A - E)
+ * |              (line2: A - B - C - D)
+ * B - C - D      (line3: C - F - G)
+ * |
+ * F - G
+ */
